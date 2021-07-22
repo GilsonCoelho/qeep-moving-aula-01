2cadde13f6717ee1c8d830d5719132a2cf3f7523
GilsonCoelho <coelho.gilson@gmail.com>
2021-07-22 18:22:29 -0300

    conclusão da revisão

--- src/br/com/qm/aula04/ExercicioSete.java
+++ src/br/com/qm/aula04/ExercicioSete.java
@@ -1,35 +1,33 @@
-  
+
 package br.com.qm.aula04;
 
 import java.util.Scanner;
 
 public class ExercicioSete {
 
-	//Escreva um programa que leia 10 números inteiros
-	//e os armazene em um vetor. Imprima o vetor, 
-	//o maior elemento e a posição que ele se encontra.
+	// Escreva um programa que leia 10 números inteiros
+	// e os armazene em um vetor. Imprima o vetor,
+	// o maior elemento e a posição que ele se encontra.
 	public static void main(String[] args) {
-		
+
 		Scanner teclado = new Scanner(System.in);
 		System.out.println("## MAIOR MENOR DENTRO DE UM VETOR ##");
 		int[] numerosLidos = new int[10];
 		int maior = 0;
 		int posicao = 0;
-		
+
 		for (int i = 0; i < numerosLidos.length; i++) {
-			System.out.println("Digite um valor: "); 
+			System.out.println("Digite um valor: ");
 			numerosLidos[i] = teclado.nextInt();
-			
+
 			if (numerosLidos[i] > maior) {
 				maior = numerosLidos[i];
 				posicao = i;
-			
 			}
-			}
-		System.out.printf("O Maior número lido é %d e ele se encontra na posição %d\n", 
-				maior, posicao);
-		
+		}
+		System.out.printf("O Maior número lido é %d e ele se encontra na posição %d\n", maior, posicao);
+
 		teclado.close();
-		
+
 	}
 }
--- src/br/com/qm/aula05/ProgramaPrincipal.java
+++ src/br/com/qm/aula05/ProgramaPrincipal.java
@@ -4,16 +4,17 @@ import java.util.Scanner;
 
 public class ProgramaPrincipal {
 
-	// 1. Crie uma função que recebe como parâmetro um 
+	// 1. Crie uma função que recebe como parâmetro um
 	// número inteiro e devolve o seu dobro.
 
-	// 2. Faça uma função que receba 3 números inteiros 
-	// como parâmetro, representando horas, minutos e segundos, e os converta em segundos.
+	// 2. Faça uma função que receba 3 números inteiros
+	// como parâmetro, representando horas, minutos e segundos, e os converta em
+	// segundos.
 
-	// 3. Faça uma função que receba uma temperatura em 
+	// 3. Faça uma função que receba uma temperatura em
 	// graus Celsius e retorne-a convertida em graus Fahrenheit.
 
-	// 4. Faça uma função que receba a altura e 
+	// 4. Faça uma função que receba a altura e
 	// o raio de um cilindro circular e retorne o volume do cilindro.
 	public static void dobro(Scanner teclado) {
 		// 96
@@ -21,44 +22,41 @@ public class ProgramaPrincipal {
 		int numero = teclado.nextInt();
 		System.out.printf("O dobro do número é: %d\n", numero * 2);
 	}
-	
+
 	/// (C × 9/5) + 32 = F
 	public static float celsiusParaFarenheit(float celsius) {
 		float farenheit = (celsius * (9 / 5F)) + 32;
 		// 41
 		return farenheit;
 	}
-	
+
 	public static void imprimeLinhas(int qtdLinhas) {
-		
+
 		for (int i = 0; i < qtdLinhas; i++) {
 			System.out.println("------------------------");
 		}
-		
+
 	}
-	
+
 	public static String menorValor(float primeiroProduto, float segundoProduto, float terceiroProduto) {
-		
-		if (primeiroProduto == segundoProduto 
-				&& segundoProduto == terceiroProduto) {
+
+		if (primeiroProduto == segundoProduto && segundoProduto == terceiroProduto) {
 			return "Tanto faz, todos os produtos tem o mesmo preço.";
-		} else if (primeiroProduto < segundoProduto 
-				&& primeiroProduto < terceiroProduto) {
+		} else if (primeiroProduto < segundoProduto && primeiroProduto < terceiroProduto) {
 			return "Compre o primeiro produto.";
-		} else if (segundoProduto < primeiroProduto 
-				&& segundoProduto < terceiroProduto) {
+		} else if (segundoProduto < primeiroProduto && segundoProduto < terceiroProduto) {
 			return "Compre o segundo produto.";
 		} else {
 			return "Compre o terceiro produto.";
 		}
-		
+
 	}
-	
+
 	public static void main(String[] args) {
-		
+
 		Scanner teclado = new Scanner(System.in);
 		System.out.println("## EXERCITANDO MENUS ##");
-		
+
 		int opcaoMenu = 0;
 		do {
 			imprimeLinhas(2);
@@ -69,37 +67,34 @@ public class ProgramaPrincipal {
 			System.out.println("Digite 0 para sair");
 			imprimeLinhas(2);
 			opcaoMenu = teclado.nextInt();
-			
+
 			switch (opcaoMenu) {
-				case 0:
-					System.out.println("Obrigado por usar o nosso programa!");
-					break;
-				case 1:
-					dobro(teclado);
-					break;
-				case 2:
-					System.out.println("Digite um valor em Celsius e eu converterei para Farenheit");
-					float tempCelsius = teclado.nextFloat();
-					float tempFarenheit = celsiusParaFarenheit(tempCelsius);
-					System.out.printf("A temperatura em Farenheit é %.2f", tempFarenheit);
-					break;
-				case 3:
-					System.out.println("Digite o primeiro valor: ");
-					float primeiroProduto = teclado.nextFloat();
-					System.out.println("Digite o segundo valor: ");
-					float segundoProduto = teclado.nextFloat();
-					System.out.println("Digite o terceiro valor: ");
-					float terceiroProduto = teclado.nextFloat();
-					System.out.println(menorValor(primeiroProduto, segundoProduto, terceiroProduto));
-				default:
-					System.out.println("Digite uma opção válida!");
+			case 0:
+				System.out.println("Obrigado por usar o nosso programa!");
+				break;
+			case 1:
+				dobro(teclado);
+				break;
+			case 2:
+				System.out.println("Digite um valor em Celsius e eu converterei para Farenheit");
+				float tempCelsius = teclado.nextFloat();
+				float tempFarenheit = celsiusParaFarenheit(tempCelsius);
+				System.out.printf("A temperatura em Farenheit é %.2f", tempFarenheit);
+				break;
+			case 3:
+				System.out.println("Digite o primeiro valor: ");
+				float primeiroProduto = teclado.nextFloat();
+				System.out.println("Digite o segundo valor: ");
+				float segundoProduto = teclado.nextFloat();
+				System.out.println("Digite o terceiro valor: ");
+				float terceiroProduto = teclado.nextFloat();
+				System.out.println(menorValor(primeiroProduto, segundoProduto, terceiroProduto));
+			default:
+				System.out.println("Digite uma opção válida!");
 			}
-			
+
 		} while (opcaoMenu != 0);
-		
-		
-		
-		
+
 //		imprimeLinhas(10);
 //		
 //		Scanner teclado = new Scanner(System.in);
@@ -114,5 +109,5 @@ public class ProgramaPrincipal {
 //		imprimeLinhas(1);
 //		teclado.close();
 	}
-	
+
 }
